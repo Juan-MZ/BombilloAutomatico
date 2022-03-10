c13726264d8a3254d5c38fcb179044a2d7d50f87
Juan José <78619790+Juan-MZ@users.noreply.github.com>
2022-03-10 18:05:03 -0500

    Delete Bombillo Automatico/MPLABXProjects directory

--- Bombillo Automatico/MPLABXProjects/PIC1.X/build/default/production/PIC.p1.d
+++
@@ -1,5 +0,0 @@
-build/default/production/PIC.p1:  \
-PIC.c  \
-led.h  \
-Config.h  \
-USART_Header_File.h 
--- Bombillo Automatico/MPLABXProjects/PIC1.X/build/default/production/_ext/1366271398/USART_Source_File.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/1366271398/USART_Source_File.p1:  \
-../../../PIC18F4550\ HC-05\ Project\ File_/HC-05_module.X/USART_Source_File.c  \
-../../../PIC18F4550\ HC-05\ Project\ File_/HC-05_module.X/USART_Header_File.h 
